f7dadf07940b5718f3e8cb12009b9210d6d0a585
JuaniVega <juanivega0206@gmail.com>
2022-02-09 18:57:02 -0300

    Primeras pantallas

--- app/src/main/java/com/example/tp_final/MainActivity.java
+++ app/src/main/java/com/example/tp_final/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.tp_final;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registrarse_p1);
     }
 }

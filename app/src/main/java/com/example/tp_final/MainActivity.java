95718f142d294a1b3883447ffa0655e9bfbe838d
Nadine Baldi <nanubaldi16@gmail.com>
2022-02-12 10:49:37 -0300

    Vista practicar preguntas en proceso

--- app/src/main/java/com/example/tp_final/MainActivity.java
+++ app/src/main/java/com/example/tp_final/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrarse_p1);
+        setContentView(R.layout.practicar_preguntas);
     }
 }
